98b53d37051469bfdd8f732d9b75dafd3d25fed0
Flavio Franco <vleix.92@gmail.com>
2022-07-10 02:05:03 -0500

    feat(*): added profile

--- app/src/main/java/com/frantun/peliandchill/presentation/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/frantun/peliandchill/presentation/ui/profile/ProfileFragment.kt
@@ -1,44 +1,6 @@
 package com.frantun.peliandchill.presentation.ui.profile
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.frantun.peliandchill.databinding.FragmentMoviesBinding
 import com.frantun.peliandchill.databinding.FragmentProfileBinding
 import com.frantun.peliandchill.presentation.common.BaseFragment
 
-class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate) {
-
-//    private var _binding: FragmentProfileBinding? = null
-//
-//    // This property is only valid between onCreateView and
-//    // onDestroyView.
-//    private val binding get() = _binding!!
-
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        val profileViewModel =
-//            ViewModelProvider(this).get(ProfileViewModel::class.java)
-//
-//        _binding = FragmentProfileBinding.inflate(inflater, container, false)
-//        val root: View = binding.root
-//
-//        val textView: TextView = binding.textHome
-//        profileViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
-//        return root
-//    }
-//
-//    override fun onDestroyView() {
-//        super.onDestroyView()
-//        _binding = null
-//    }
-}
+class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate)
